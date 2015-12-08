7f4c30e3abcbeb9364be8d27f6436bd2b053d129
Nikita <nikitashr@gmail.com>
2015-12-08 15:51:41 +0545

    First commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def services
+  end
+
+  def portfolio
+  end
+
+  def team
+  end
+
+  def contact
+  end
+end
