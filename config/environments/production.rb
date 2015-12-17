6e6c1f5d63bc7da7d97e450c6423f395b029a5ca
Nikita <nikitashr@gmail.com>
2015-12-17 11:59:07 +0545

    set the secret key using the environment variable instead of looking for it in secrets.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # This tells your application to set the secret key using the environment variable instead of looking for it in secrets.yml. 
+  # secrets.yml should never be in your repo like GitHub or Heroku
+  # Github puts these files in .gitignore for security.
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
 end
